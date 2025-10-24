0eedff361fd94ed20df58c1e7fee363c3d8ba1e8
Maria <mariaaguedez.08@gmail.com>
2025-10-24 16:32:38 -0400

    feat: Agregar script para iniciar el servidor en Docker y corregir la configuración del entorno para el entorno requerido del contenedor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,14 +16,11 @@ export const environment = {
   // Url: 'http://180.183.67.228:8080/api',
   // Url: 'http://180.183.67.228:9091/api',
   // Url: 'http://180.183.67.212:8081/api',
+  // Url: 'http://180.183.67.228:8081/api',
   Url: 'localhost:8081/api',
 
   API: '/api/',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
-  nombreAmbiente: 'LOCAL'
+  nombreAmbiente: 'DESARRLLO'
 
 };
-
-// export const environment = {
-//   urlEndpoint: 'http://180.183.67.228:8080/api',
-// }
